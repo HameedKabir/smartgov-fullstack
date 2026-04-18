d0001e45c7330d09d8f22a6448197f55bc275c6c
Kabir Hameed <hameedkabir6@gmail.com>
2026-04-18 18:01:25 +0100

    Remove all files from repository

--- backend/src/utils/token.js
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-
-import { env } from "../config/env.js";
-
-export const generateToken = (payload) =>
-  jwt.sign(payload, env.jwtSecret, { expiresIn: env.jwtExpiresIn });
